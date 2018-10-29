ee699fe65d3a07026c75aa650ead2abe8c0f9a15
wang120140 <1011048154@qq.com>
2018-10-29 20:52:14 +0800

    Signed-off-by: wang120140 <1011048154@qq.com>添加HTTP具体信息

--- src/components/HttpComponent.vue
+++ src/components/HttpComponent.vue
@@ -78,7 +78,74 @@
             UDP的首部开销小,只有8个字节,比TCP的20个字节的首部要短
         <h3>HTTP协议</h3>
             <img src="../../public/img/http05png.png" alt="">
+        <h3>常用的请求头字段的含义</h3>
+            Accept 设置接受的内容类型
+                Accept: text/plain
+            Accept-Charset 设置接受的字符编码
+                Accept-Charset: utf-8
+            Accept-Encoding 设置接受的编码格式
+                Accept-Encoding: gzip, deflate
+            Accept-Datetime 设置接受的版本时间
+                Accept-Datetime: Thu, 31 May 2007 20:35:00 GMT
+            Accept-Language 设置接受的语言
+                Accept-Language: en-US
+            Authorization 设置HTTP身份验证的凭证
+                Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==
+            Cache-Control 设置请求响应链上所有的缓存机制必须遵守的指令
+                Cache-Control: no-cache
+            Connection 设置当前连接和hop-by-hop协议请求字段列表的控制选项
+                Connection: keep-alive
+                Connection: Upgrade
+            Content-Length 设置请求体的字节长度
+                Content-Length: 348
+            Content-MD5 设置基于MD5算法对请求体内容进行Base64二进制编码
+                Content-MD5: Q2hlY2sgSW50ZWdyaXR5IQ==
+            Content-Type 设置请求体的MIME类型（适用POST和PUT请求）
+                Content-Type: application/x-www-form-urlencoded
+            Cookie 设置服务器使用Set-Cookie发送的http cookie
+                Cookie: $Version=1; Skin=new;
+            Date 设置消息发送的日期和时间
+                Date: Tue, 15 Nov 1994 08:12:31 GMT
+            Expect 标识客户端需要的特殊浏览器行为
+                Expect: 100-continue
+            Forwarded 披露客户端通过http代理连接web服务的源信息
+                Forwarded: for=192.0.2.60;proto=http;by=203.0.113.43
+            From 设置发送请求的用户的email地址
+                From: user@example.com
+            Host 设置服务器域名和TCP端口号，如果使用的是服务请求标准端口号，端口号可以省略
+                Host: en.wikipedia.org:8080
+            If-Match 设置客户端的ETag,当时客户端ETag和服务器生成的ETag一致才执行，适用于更新自从上次更新之后没有改变的资源
+                If-Match: "737060cd8c284d8af7ad3082f209582d
+            If-Modified-Since 设置更新时间，从更新时间到服务端接受请求这段时间内如果资源没有改变，允许服务端返回304 Not Modified
+                If-Modified-Since: Sat, 29 Oct 1994 19:43:31 GMT
+            If-None-Match 设置客户端ETag，如果和服务端接受请求生成的ETage相同，允许服务端返回304 Not Modified
+                If-None-Match: "737060cd8c284d8af7ad3082f209582d"
+            If-Range 设置客户端ETag，如果和服务端接受请求生成的ETage相同，返回缺失的实体部分；否则返回整个新的实体
+                If-Range: "737060cd8c284d8af7ad3082f209582d"
+            If-Unmodified-Since 设置更新时间，只有从更新时间到服务端接受请求这段时间内实体没有改变，服务端才会发送响应
+                If-Unmodified-Since: Sat, 29 Oct 1994 19:43:31 GMT
+            Max-Forwards 限制代理或网关转发消息的次数
+                Max-Forwards: 10
+            Origin 标识跨域资源请求（请求服务端设置Access-Control-Allow-Origin响应字段
+                Origin: http://www.example-social-network.com
+            Pragma 设置特殊实现字段，可能会对请求响应链有多种影响
+                Pragma: no-cache
+            Proxy-Authorization 为连接代理授权认证信息
+                Proxy-Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==
+            User-Agent 用户代理的字符串值
+                User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/21.0
+            Via 通知服务器代理请求
+                Via: 1.0 fred, 1.1 example.com (Apache/1.1)
+            <h4>
+                常用非标准请求头字段
+            </h4>
+            <hr>
+            X-Requested-With 标识Ajax请求，大部分js框架发送请求时都会设置它为XMLHttpRequest
+                X-Requested-With: XMLHttpRequest
+
+
             </pre>
+            <a href="https://www.jianshu.com/p/6e86903d74f7">参考链接</a>
     </div>
 </template>
 
